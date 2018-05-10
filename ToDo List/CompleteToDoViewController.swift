eda8ae742ba7de1fa96905bc34debeef0497edbc
Anil Patel <patel_anil@hotmail.com>
2018-05-11 00:12:12 +0100

    Up to point before introducing CoreData - lesson 66

---
+++ ToDo List/CompleteToDoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CompleteToDoViewController.swift
+//  ToDo List
+//
+//  Created by Anil Patel on 10/05/2018.
+//  Copyright © 2018 Anil Patel. All rights reserved.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+
+    var previousVC = ToDoTableViewController()
+    var selectedToDo = ToDo()
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = selectedToDo.name
+    }
+
+    @IBAction func completeTapped(_ sender: Any) {
+        var index = 0
+        for toDo in previousVC.toDos {
+            if toDo.name == selectedToDo.name {
+                previousVC.toDos.remove(at: index)
+                previousVC.tableView.reloadData()
+                navigationController?.popViewController(animated: true)
+                break
+            }
+            index += 1
+        }
+    }
+}
+
