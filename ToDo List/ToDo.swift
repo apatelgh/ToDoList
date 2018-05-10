33c45cc634550e7162a486a8a766c307378f5fa4
Anil Patel <patel_anil@hotmail.com>
2018-05-10 15:16:37 +0100

    ToDo List repo created

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Anil Patel on 10/05/2018.
+//  Copyright © 2018 Anil Patel. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ ToDo List/ToDoTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ToDoTableViewController.swift
+//  ToDo List
+//
+//  Created by Anil Patel on 10/05/2018.
+//  Copyright © 2018 Anil Patel. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDos()
+    }
+    
+    func createToDos() -> [ToDo] {
+        
+        let eggs = ToDo()
+        eggs.name = "Buy Eggs"
+        eggs.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        
+        let cheese = ToDo()
+        cheese.name = "Eat Cheese"
+        
+        return [eggs, dog, cheese]
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        
+        return cell
+    }
+
+}
